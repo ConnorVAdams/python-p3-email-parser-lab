59e3e5c5007f4b3af353f5929f46991e6b67a02a
Connor Adams <connorvadams@gmail.com>
2023-10-25 21:13:14 -0600

    Submits lab.

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,11 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+    
+    def parse(self):
+        split_regex = r'\s|,\s|,'
+        emails_to_parse = sorted(re.split(split_regex, self.email_addresses))
+        validate_regex = re.compile(r"[A-z][A-z.0-9]*@[a-z]*.[a-z]*\b")
+        return [email for email in emails_to_parse if validate_regex.match(email)]
